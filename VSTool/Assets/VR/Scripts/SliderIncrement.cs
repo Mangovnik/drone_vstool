9965a4de68d103a59759b10fbe094369f8d3b304
David Sladký <davsladk@gmail.com>
2021-03-29 21:39:02 +0200

    Added increment and decrement buttons to setting

---
+++ VSTool/Assets/VR/Scripts/SliderIncrement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderIncrement : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    public void Increment()
+    {
+        slider.value += 1.0f;
+    }
+
+    public void Decrement()
+    {
+        slider.value -= 1.0f;
+    }
+}
