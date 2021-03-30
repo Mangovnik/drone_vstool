08591cdd5fa4ee1de6ed4b280c7c9c3bba860b00
David Sladký <davsladk@gmail.com>
2021-03-30 03:06:43 +0200

    Added UI interactor for camera shortcuts

---
+++ VSTool/Assets/VR/Scripts/LeftUIInteractorToggle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LeftUIInteractorToggle : MonoBehaviour
+{
+    public GameObject leftUIInteractor;
+
+    public InputAction showInteractor;
+    public InputAction hideInteractor;
+
+    private void OnEnable()
+    {
+        showInteractor.Enable();
+        hideInteractor.Enable();
+    }
+
+    private void OnDisable()
+    {
+        showInteractor.Disable();
+        hideInteractor.Disable();
+    }
+
+    private void Awake()
+    {
+        showInteractor.performed += showInteractorAction;
+        hideInteractor.performed += hideInteractorAction;
+        showInteractor.canceled += hideInteractorAction;
+    }
+
+    private void hideInteractorAction(InputAction.CallbackContext obj)
+    {
+        leftUIInteractor.SetActive(false);
+    }
+
+    private void showInteractorAction(InputAction.CallbackContext obj)
+    {
+        leftUIInteractor.SetActive(true);
+    }
+}
