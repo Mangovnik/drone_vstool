25661dfae2dfd4af909da273c35459329a9cd859
David Sladký <davsladk@gmail.com>
2021-04-16 00:04:01 +0200

    Added teleportation movement

--- VSTool/Assets/VR/Scripts/CustomTeleportMovement.cs
+++ VSTool/Assets/VR/Scripts/CustomTeleportMovement.cs
@@ -1,37 +1,70 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using System;
 
 public class CustomTeleportMovement : MonoBehaviour
 {
-    public float maximalTeleportDistance = 10.0f;
     public Slider maximalTeleportSlider;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public GameObject ui;
+    public Text text;
+    public Transform teleportTarget;
+    public InputAction distance;
+    public InputAction activate;
+    public InputAction teleport;
+
+    private bool activated = false;
+
+    private void Awake() {
+        activate.performed += activateAction;
+        activate.canceled += activateCanceled;
+        teleport.performed += teleportAction;
+    }
+
+    private void teleportAction(InputAction.CallbackContext obj) {
+        transform.position = teleportTarget.position;
+    }
+
+    private void activateCanceled(InputAction.CallbackContext obj) {
+        teleportTarget.gameObject.SetActive(false);
+        ui.SetActive(false);
+        teleportTarget.localPosition = Vector3.zero;
+        activated = false;
+    }
+
+    private void activateAction(InputAction.CallbackContext obj) {
+        teleportTarget.gameObject.SetActive(true);
+        ui.SetActive(true);
+        activated = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activated) {
+            Vector2 stickValue = distance.ReadValue<Vector2>();
+            float tmp = stickValue.magnitude * maximalTeleportSlider.value;
+            teleportTarget.localPosition = Vector3.forward * tmp;
+
+            text.text = Vector3.Distance(transform.position, teleportTarget.position).ToString("0.0") + " m";
+        }
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         Debug.Log("Teleport disabled.");
+        distance.Disable();
+        activate.Disable();
+        teleport.Disable();
     }
 
     private void OnEnable()
     {
         Debug.Log("Teleport enabled.");
-    }
-
-    public void setMaximalTeleportDistanceFromUI()
-    {
-        maximalTeleportDistance = maximalTeleportSlider.value;
-        Debug.Log(maximalTeleportDistance);
+        distance.Enable();
+        activate.Enable();
+        teleport.Enable();
     }
 }
